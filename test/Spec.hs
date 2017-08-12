db8a7d4349417e6097234ae53be9d46752936d88
d61h6k4 <dbihbka@protonmail.ch>
2017-08-12 10:43:04 +0300

    Add example how work with module

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,27 @@
+{-# LANGUAGE OverloadedStrings #-}
+import System.IO (stdout)
+import Control.Lens ((<&>), (.~))
+import Control.Monad.Trans.Resource (runResourceT)
+
+import qualified Data.Machine as Machine
+import qualified Network.Google as Google
+import qualified Network.Google.Storage as Storage
+
+import Data.Machine.Source.Google.Storage
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  lgr <- Google.newLogger Google.Debug stdout
+  env <-
+    Google.newEnv <&> (Google.envLogger .~ lgr) .
+    (Google.envScopes .~ Storage.storageReadWriteScope)
+  parseObjectName env "erregistro" "company/*/project/*/service/*/2017/08/07/" >>=
+    print
+  runResourceT
+    (Machine.runT
+       ((Machine.construct
+           (fromGCS
+              env
+              "erregistro"
+              "company/*/project/*/service/*/2017/08/07/")))) >>= print
+  return ()
